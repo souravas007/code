e59e9391bcae85cb034c76a6d4f80831c1325727
sourav <souravas007@gmail.com>
2024-03-15 17:25:27 +0530

    Add encode_decode

---
+++ neetcode/arrays_hashes/encode_decode.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    def encode(self, strs: list[str]) -> str:
+        encoded_string = ""
+        for string in strs:
+            encoded_string += f"{len(string)}#{string}"
+        return encoded_string
+
+    def decode(self, s: str) -> list[str]:
+        result = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != '#':
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            result.append(s[i:j])
+            i = j
+        return result
+   
