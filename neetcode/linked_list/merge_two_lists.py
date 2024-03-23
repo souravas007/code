b2848e89f6fd12aca0a6b4cb8c50f001e8421bc2
sourav <souravas007@gmail.com>
2024-03-23 17:57:26 +0530

    Add merge_two_lists

---
+++ neetcode/linked_list/merge_two_lists.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from neetcode.linked_list.list_node import ListNode
+
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        result: ListNode = ListNode()
+        curr = result
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        if list1:
+            curr.next = list1
+        if list2:
+            curr.next = list2
+        return result.next
+
+
