2afd2431df04affca638721ae567d5fe5491c360
sourav <souravas007@gmail.com>
2024-03-27 13:13:33 +0530

    Add same_tree

---
+++ neetcode/trees/same_tree.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+from neetcode.trees.tree_node import TreeNode
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
