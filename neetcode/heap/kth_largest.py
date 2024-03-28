678fe32db920cf230a89d3912969333b4e6c2e44
sourav <souravas007@gmail.com>
2024-03-28 16:27:42 +0530

    Add kth_largest

---
+++ neetcode/heap/kth_largest.py
@@ -0,0 +1,17 @@
+import heapq
+
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: list[int]):
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap)
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
